06888f1b8ea7d1dd8fa2521e1d508140aa57a168
Brian Schick <brian_s@beezwax.net>
2019-05-11 15:09:43 -0700

    Complete Section 5 assets

---
+++ For Student/5 Advanced/5.06 Slack/Beginning/build/ChewChewTrain.build/Release-iphoneos/ChewChewTrain.build/DerivedSources/ChewChewTrain_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ChewChewTrainVersionString[];
+ extern const double ChewChewTrainVersionNumber;
+
+ const unsigned char ChewChewTrainVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ChewChewTrain  PROJECT:ChewChewTrain-26" "\n";
+ const double ChewChewTrainVersionNumber __attribute__ ((used)) = (double)26.;
---
+++ For Student/5 Advanced/5.07 Plugins/Beginning/build/ChewChewTrain.build/Release-iphoneos/ChewChewTrain.build/Objects-normal/arm64/ChewChewTrain_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/brian/Desktop/ChewChewTrain/build/ChewChewTrain.build/Release-iphoneos/ChewChewTrain.build/DerivedSources/ChewChewTrain_vers.c
